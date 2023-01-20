3c502cb257906cea70ae43926d48d1b8aa9f742e
maryhmn <maryam.h.ettefagh@gmail.com>
2023-01-21 03:01:18 +0330

    update current ttim

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Thusday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[date.getDay()];
+
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute}`;
+}
 function showTempreture(response) {
   console.log(response.data);
-  console.log(response.data.city);
   document.querySelector("#city-name").innerHTML = response.data.city;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.temperature.current
@@ -11,6 +33,9 @@ function showTempreture(response) {
     response.data.temperature.humidity;
   document.querySelector("#speed").innerHTML = Math.round(
     response.data.wind.speed
+  );
+  document.querySelector("#currentDate").innerHTML = formatDate(
+    response.data.time * 1000
   );
 }
 let query = "Lisbon";
